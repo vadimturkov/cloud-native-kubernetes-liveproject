92835638788970509be3e7aa0ca0f653b2a01188
Vadim Turkov <vadim.turkov@gmail.com>
2020-10-24 23:55:28 +0300

    milestone-1: Add and externalize secrets

--- payments/repositories/index.js
+++ payments/repositories/index.js
@@ -3,7 +3,8 @@ let makeRedisClient = (config) => {
     const redis = require("async-redis")
     const options = {
         host: config.redis_host,
-        port: config.redis_port
+        port: config.redis_port,
+        password: process.env.REDIS_PASSWORD
     }
 
     return redis.createClient(options)
